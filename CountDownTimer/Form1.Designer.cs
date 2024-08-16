7ae05531e764da509c08ae494bb70517a509c41b
nkturtas@gmx.com <nkturtas@gmx.com>
2024-08-16 11:25:09 +0300

    Add project files.

---
+++ CountDownTimer/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CountDownTimer
+{
+    partial class CountDownTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.topLabel = new System.Windows.Forms.Label();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLabel
+            // 
+            this.topLabel.Font = new System.Drawing.Font("Arial Nova", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.Location = new System.Drawing.Point(12, 24);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(776, 62);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.Text = "Count Down Timer";
+            this.topLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.Font = new System.Drawing.Font("Arial Nova", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.Location = new System.Drawing.Point(12, 86);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(776, 101);
+            this.timerLabel.TabIndex = 1;
+            this.timerLabel.Text = "60 Seconds";
+            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.White;
+            this.stopButton.Font = new System.Drawing.Font("Arial Nova Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(398, 222);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(149, 80);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.White;
+            this.startButton.Font = new System.Drawing.Font("Arial Nova Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(238, 308);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(309, 80);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // plusButton
+            // 
+            this.plusButton.BackColor = System.Drawing.Color.White;
+            this.plusButton.Font = new System.Drawing.Font("Arial Nova Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusButton.Location = new System.Drawing.Point(238, 392);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(115, 55);
+            this.plusButton.TabIndex = 5;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = false;
+            this.plusButton.Click += new System.EventHandler(this.plusButton_Click);
+            // 
+            // minusButton
+            // 
+            this.minusButton.BackColor = System.Drawing.Color.White;
+            this.minusButton.Font = new System.Drawing.Font("Arial Nova Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusButton.Location = new System.Drawing.Point(432, 392);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(115, 55);
+            this.minusButton.TabIndex = 6;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = false;
+            this.minusButton.Click += new System.EventHandler(this.minusButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.White;
+            this.resetButton.Font = new System.Drawing.Font("Arial Nova Cond", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(243, 222);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(149, 80);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click_1);
+            // 
+            // CountDownTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(812, 476);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.topLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CountDownTimer";
+            this.Text = "Count Down Timer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button minusButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ CountDownTimer/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownTimer
+{
+    public partial class CountDownTimer : Form
+    {
+        public CountDownTimer()
+        {
+            InitializeComponent();
+        }
+
+        int timeLeft = 60;
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timerLabel.Text = timeLeft + " Seconds";
+            }
+            else
+            {
+                timer.Stop();
+                timerLabel.Text = "Time is out";
+            }
+        }
+
+        private void resetButton_Click_1(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timeLeft = 60;
+            timerLabel.Text = timeLeft + " Seconds";
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            timeLeft = timeLeft + 1;
+            timerLabel.Text = timeLeft.ToString() + " Seconds";
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            timeLeft = timeLeft - 1;
+            timerLabel.Text = timeLeft.ToString() + " Seconds";
+        }
+
+       
+    }
+}
